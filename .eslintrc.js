92cc989e18246ecb9638a3781f30c2008fd5862e
Johnson <zxmagichacker@gmail.com>
2020-09-23 16:18:03 +0800

    feat: 配置eslint
    引入attack和prettier集成eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,13 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["attack", "attack/vue", "prettier"],
+  plugins: ['prettier'],
   parserOptions: {
     parser: "babel-eslint"
   },
   rules: {
+    'prettier/prettier': 'error',
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   },
