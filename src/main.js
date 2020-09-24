5fc9b5755929c039714d67fb423d872bdc0e7dc0
Johnson <zxmagichacker@gmail.com>
2020-09-24 17:36:16 +0800

    feat: 引入layout，menu组件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import { Button, Layout, Menu, Icon } from 'ant-design-vue'
+;[Button, Layout, Menu, Icon].forEach(item => {
+  Vue.use(item)
+})
 Vue.config.productionTip = false
 
 new Vue({
