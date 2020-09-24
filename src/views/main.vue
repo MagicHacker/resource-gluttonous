6385177d82ae7e9028a4ff3e74eb82513aaccfb0
Johnson <zxmagichacker@gmail.com>
2020-09-24 17:36:45 +0800

    feat: 构建main组件

--- src/views/main.vue
+++ src/views/main.vue
@@ -1,9 +1,56 @@
 <template>
-  <div>主页</div>
+  <a-layout class="layout-wrap">
+    <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
+      <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
+        <a-menu-item key="1">
+          <a-icon type="picture" />
+          <span>图片</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="audio" />
+          <span>音频</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <a-icon type="deployment-unit" />
+          <span>动效</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header class="main-header">
+        <a-icon
+          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+          @click="menuTrigger"
+        ></a-icon>
+      </a-layout-header>
+      <a-layout-content>
+        content
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
 <script>
 export default {
-  name: 'mainComponent'
+  name: 'mainComponent',
+  data() {
+    return {
+      collapsed: true
+    }
+  },
+  methods: {
+    menuTrigger() {
+      this.collapsed = !this.collapsed
+    }
+  }
 }
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.layout-wrap {
+  width: 100%;
+  height: 100%;
+  font-size: 16px;
+  .main-header {
+    background: #fff;
+  }
+}
+</style>
