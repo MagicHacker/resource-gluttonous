014ee124c37446c2f448968f5e6a3928c4e6129f
Johnson <zxmagichacker@gmail.com>
2020-09-26 22:13:36 +0800

    feat: 构建音效页

--- src/views/audios.vue
+++ src/views/audios.vue
@@ -1,3 +1,94 @@
 <template>
-  <div>音频页</div>
+  <div class="audios-wrapper">
+    <div class="audios-search">
+      <a-form-model layout="inline">
+        <a-form-model-item label="项目">
+          <a-input placeholder="项目名称"></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="资源名">
+          <a-input placeholder="资源名"></a-input>
+        </a-form-model-item>
+        <a-form-model-item>
+          <a-button type="primary">搜索</a-button>
+        </a-form-model-item>
+      </a-form-model>
+    </div>
+    <div class="audios-add">
+      <a-button type="success">添加</a-button>
+    </div>
+    <div class="audios-table">
+      <a-table bordered :data-source="tableData" :columns="columns">
+        <template v-slot:action>
+          <div class="audios-table-action">
+            <a-button type="primary">详情</a-button>
+            <a-button type="danger">删除</a-button>
+          </div>
+        </template>
+      </a-table>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  name: 'audiosComponent',
+  data() {
+    return {
+      tableData: [
+        {
+          key: '0',
+          project: 'Edward King 0',
+          sourceName: '32',
+          path: 'London, Park Lane no. 0'
+        },
+        {
+          key: '1',
+          project: 'Edward King 1',
+          sourceName: '32',
+          path: 'London, Park Lane no. 1'
+        }
+      ],
+      columns: [
+        {
+          title: '项目',
+          align: 'center',
+          dataIndex: 'project'
+        },
+        {
+          title: '资源名',
+          dataIndex: 'sourceName'
+        },
+        {
+          title: '路径',
+          dataIndex: 'path'
+        },
+        {
+          title: '操作',
+          dataIndex: 'action',
+          scopedSlots: { customRender: 'action' }
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.audios-wrapper {
+  width: 100%;
+  height: 100%;
+  padding: 20px 0px 0px 30px;
+  .audios-search {
+    width: 100%;
+    height: 60px;
+  }
+  .audios-add {
+    width: 100%;
+    height: 50px;
+  }
+  .audios-table-action {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+}
+</style>
