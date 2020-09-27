eae6aeda61e8e99e862ad27003649beeac54f4ed
Johnson <zxmagichacker@gmail.com>
2020-09-27 15:46:06 +0800

    feat: 构建模态框

---
+++ src/components/pop-modal.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="pop-modal-wrapper">
+    <a-modal
+      v-model="showModal"
+      title="title"
+      :destroyOnClose="true"
+      :footer="null"
+      @cancel="handleCancel"
+    ></a-modal>
+  </div>
+</template>
+<script>
+export default {
+  name: 'popModal',
+  data() {
+    return {
+      showModal: false
+    }
+  },
+  props: {},
+  methods: {
+    showModalFunc() {
+      this.showModal = true
+    },
+    handleCancel() {}
+  }
+}
+</script>
+<style lang="less" scoped>
+.pop-modal-wrapper {
+  width: 100%;
+}
+</style>
