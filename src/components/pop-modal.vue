8b670f1a9426d23cb700a57104e71f4988625736
Johnson <zxmagichacker@gmail.com>
2020-09-27 23:53:03 +0800

    feat: 模态框添加内容

--- src/components/pop-modal.vue
+++ src/components/pop-modal.vue
@@ -2,11 +2,24 @@
   <div class="pop-modal-wrapper">
     <a-modal
       v-model="showModal"
-      title="title"
+      title="资源清单"
+      width="600px"
       :destroyOnClose="true"
       :footer="null"
       @cancel="handleCancel"
-    ></a-modal>
+    >
+      <a-form-model layout="vertical">
+        <a-form-model-item label="项目">
+          <a-input placeholder="项目名称"></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="资源名">
+          <a-input placeholder="资源名"></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="路径">
+          <a-input placeholder="路径"></a-input>
+        </a-form-model-item>
+      </a-form-model>
+    </a-modal>
   </div>
 </template>
 <script>
